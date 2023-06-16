1ea29466b893feec1db0055002572f837048a90f
Damien White <damien.white@visoftinc.com>
2023-06-15 20:18:44 -0400

    fix: Remove warning for Android

--- android/src/main/java/com/meedan/ShareMenuModule.java
+++ android/src/main/java/com/meedan/ShareMenuModule.java
@@ -163,4 +163,15 @@ public class ShareMenuModule extends ReactContextBaseJavaModule implements Activ
     // Update intent in case the user calls `getSharedText` again
     currentActivity.setIntent(intent);
   }
+  
+  // Remove warnings for Android
+  @ReactMethod
+  public void addListener(String eventName) {
+
+  }
+
+  @ReactMethod
+  public void removeListeners(Integer count) {
+
+  }
 }
